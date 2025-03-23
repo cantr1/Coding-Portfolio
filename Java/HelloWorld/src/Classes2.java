e4ee8918f6a90e32d41be011cc714bdb194ee713
Kelly Cantrell <109242715+cantr1@users.noreply.github.com>
2025-03-23 13:51:58 -0500

    Removing the basics stuff - do not need

--- Java/HelloWorld/src/Classes2.java
+++
@@ -1,8 +0,0 @@
-public class Classes2 {
-    public static void main(String[] args) {
-        Classes car = new Classes(); //Import from the Classes file
-        car.setMake("Porsche"); //Setter
-        car.setConvertable(true);
-        car.describeCar();
-    }
-}
--- Java/HelloWorld/src/Constructors2.java
+++
@@ -1,9 +0,0 @@
-public class Constructors2 {
-    public static void main(String[] args) {
-        Constructors func = new Constructors();
-        // Constructors func = new Constructors("kelz", "333");
-
-        System.out.println("Name: " + func.getName());
-        System.out.println("Number: " + func.getPhone());
-    }
-}
