dcacea3319d89a9467e9e80794fd86e9bc9c7757
Kelly Cantrell <kellycantrelly@gmail.com>
2026-04-23 16:04:39 -0500

    Updating basic functions

--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -5,3 +5,98 @@ const isOnline: boolean = true;
 const noValue: null = null;
 const notDefined: undefined = undefined;
 
+// Unions
+function safeSquare(val: string | number): number {
+  if (typeof val === "string") {
+    val = parseInt(val, 10);
+  }
+  // now val is only a number
+  return val * val;
+}
+
+let result = safeSquare("5");
+console.log(result);
+// 25
+
+result = safeSquare(5);
+console.log(result);
+// 25
+
+// Optional Parameters
+function greet(name: string, title?: string): string {
+  if (title) {
+    return `Hello, ${title} ${name}!`;
+  }
+  return `Hello, ${name}!`;
+}
+
+greet("Gandalf");           // "Hello, Gandalf!"
+greet("Gandalf", "Wizard"); // "Hello, Wizard Gandalf!"
+
+// Literal Types
+//function move(direction: "north") {
+  // Implementation...
+//}
+
+type Direction = "north" | "south" | "east" | "west";
+
+function move(direction: Direction) {
+  // Implementation...
+}
+
+/* for simple return statements
+if (level === "low") return 1;
+*/
+
+type Class = "wizard" | "warrior" | "rogue";
+type Race = "elf" | "human" | "dwarf";
+type Hero = `Hero: ${Race} ${Class}`;
+// Hero: elf wizard | Hero: elf warrior and so on...
+
+// easy iteration with .forEach()
+
+// discriminated unions
+type MultipleChoiceLesson = {
+  kind: "multiple-choice"; // Discriminant property
+  question: string;
+  studentAnswer: string;
+  correctAnswer: string;
+};
+
+type CodingLesson = {
+  kind: "coding"; // Discriminant property
+  studentCode: string;
+  solutionCode: string;
+};
+
+type Lesson = MultipleChoiceLesson | CodingLesson;
+
+function isCorrect(lesson: Lesson): boolean {
+  switch (lesson.kind) {
+    case "multiple-choice":
+      return lesson.studentAnswer === lesson.correctAnswer;
+    case "coding":
+      return lesson.studentCode === lesson.solutionCode;
+  }
+}
+
+// Sets
+// A Set that contains only strings
+const justiceLeague = new Set<string>();
+
+justiceLeague.add("Green Arrow");
+justiceLeague.add("Flash");
+
+// Error: Argument of type '2' is not assignable to parameter of type 'string'
+justiceLeague.add(2);
+
+const justiceLeague = new Set<string>(["Atom", "Black Canary", "Blue Beetle"]);
+
+console.log(justiceLeague.size); // 3
+
+justiceLeague.delete("Blue Beetle");
+console.log(justiceLeague.has("Blue Beetle")); // false
+
+justiceLeague.forEach((member) => console.log(member));
+// Atom
+// Black Canary
