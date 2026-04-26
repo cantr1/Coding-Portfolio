751cc5eb8769c2163fef55a98979609810cb15fc
Kelly Cantrell <kellycantrelly@gmail.com>
2026-04-26 17:12:40 -0500

    Adding test project for local dev work

---
+++ TypeScript/local_dev_project/index.js
@@ -0,0 +1,7 @@
+import { log, chats } from "./chats.js";
+const button = document.getElementById("enable-button");
+button.addEventListener("click", () => {
+    window.supportAI.enableAutoReply();
+});
+console.log("Hello, World!");
+log(chats);
