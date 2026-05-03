1de2f39084ca845437a360730219957a5036bc03
Kelly Cantrell <kellycantrelly@gmail.com>
2026-05-03 14:07:38 -0500

    Starting http_clients study in TS

---
+++ TypeScript/http_clients.ts
@@ -0,0 +1,7 @@
+// URL API is built into JS
+// Create a URL object
+const urlObj = new URL("https://homestarrunner.com/toons");
+
+// Access properties
+console.log(urlObj.hostname); // "homestarrunner.com"
+console.log(urlObj.pathname); // "/toons"
