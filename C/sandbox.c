c3bdaa2de47e7cf808038b2879162f9c788ca182
Kelly Cantrell <kellycantrelly@gmail.com>
2026-05-06 19:46:56 -0500

    Playing with C

--- C/sandbox.c
+++ C/sandbox.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+typedef struct {
+    char* name;
+    int user_id;
+} t_user;
+
+typedef struct {
+    int process_id;
+    t_user user;
+    int result;
+} t_calculation;
+
+t_user new_user(char* name) {
+    t_user obj = {name, 1};
+    return obj;
+}
+
 int main() {
-    printf("Hello, ");
-    printf("World!");
-    printf("\a");
+    char name[30];
+
+    printf("What is your name: ");
+    getchar();
+    fgets(name, sizeof(name), stdin);
+
+    t_user user_object = new_user(name);
+
+    
+
     return 0;
 }
