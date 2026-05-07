3f71a0b91dd9f2cd4603a56bf2e27d4ae1a8ec46
Kelly Cantrell <kellycantrelly@gmail.com>
2026-05-07 16:33:33 -0500

    Experimenting with typedef and pointers

--- C/sandbox.c
+++ C/sandbox.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char* name;
@@ -11,21 +12,22 @@ typedef struct {
     int result;
 } t_calculation;
 
-t_user new_user(char* name) {
-    t_user obj = {name, 1};
-    return obj;
+t_user *new_user(char* name) {
+    t_user *objPtr = malloc(sizeof(t_user));
+    objPtr->name = name;
+    objPtr->user_id = 1;
+    return objPtr;
 }
 
 int main() {
     char name[30];
 
     printf("What is your name: ");
-    getchar();
     fgets(name, sizeof(name), stdin);
 
-    t_user user_object = new_user(name);
+    t_user *user_object = new_user(name);
 
-    
+    printf("Hello, %s", user_object->name);
 
     return 0;
 }
