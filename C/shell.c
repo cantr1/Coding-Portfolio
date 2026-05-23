1592828ac44c2a4dc57fb14963816948c6d6ddb7
Kelly Cantrell <kellycantrelly@gmail.com>
2026-05-23 15:20:54 -0500

    working on a C shell project

---
+++ C/shell.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *get_command(char *buffer, size_t size) {
+    return fgets(buffer, size, stdin);
+}
+
+int main() {
+    while (1) {
+        printf("c_shell:~$ ");
+
+        char cmd[1024];
+        get_command(cmd, sizeof(cmd));
+        cmd[strcspn(cmd, "\n")] = '\0';   // remove newline
+
+        if (strcmp(cmd, "exit") == 0) {
+            exit(0);
+        }
+
+        // For debugging, will remove later
+        printf("Entered command: %s\n", cmd);
+    }
+    return 0;
+}
