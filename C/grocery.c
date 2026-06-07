736c77863284bae1d48502bb25658f353f878cf9
Kelly Cantrell <kellycantrelly@gmail.com>
2026-06-07 14:38:45 -0500

    continue with the grocery project

--- C/grocery.c
+++ C/grocery.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct GroceryList grocery_t;
 typedef struct Recipe recipe_t;
@@ -7,14 +8,14 @@ typedef struct Ingredient ingredient_t;
 typedef struct GroceryList {
     recipe_t **recipes;
     float total_cost;
-    int recipe_count;
+    size_t recipe_count;
 } grocery_t;
 
 typedef struct Recipe {
     char *name;
-    ingredient_t **ingredient;
+    ingredient_t **ingredients;
     float recipe_cost;
-    int num_ingredients;
+    size_t num_ingredients;
 } recipe_t;
 
 typedef struct Ingredient {
@@ -29,26 +30,30 @@ void calculate_total_cost(grocery_t *g) {
 
     g->total_cost = 0.0;
 
-    for (int i = 0; i < g->recipe_count; i++) {
-        calculate_recipe_cost(g->recipes[i]);
+    for (size_t i = 0; i < g->recipe_count; i++) {
+        //calculate_recipe_cost(g->recipes[i]);
         g->total_cost = g->total_cost + g->recipes[i]->recipe_cost;
     }
 
     return;
 }
 
-void calculate_recipe_cost(recipe_t *r) {
+float calculate_recipe_cost(recipe_t *r) {
     if (r == NULL) {
-        return;
+        return 0.0;
     }
 
-    r->recipe_cost = 0.0;
+    printf("Recipe: %s\n", r->name);
 
-    for (int i = 0; i < r->num_ingredients; i++) {
-        r->recipe_cost = r->recipe_cost + r->ingredient[i]->cost;
+    float recipe_cost = 0.0;
+
+    for (size_t i = 0; i < r->num_ingredients; i++) {
+        printf("Ingredient %s costs: %.2f\n", r->ingredients[i]->name, r->ingredients[i]->cost);
+        recipe_cost += r->ingredients[i]->cost;
     }
 
-    return;
+    printf("Total Cost: %.2f\n", recipe_cost);
+    return recipe_cost;
 }
 
 ingredient_t *create_ingredient(char *name, float cost) {
@@ -59,8 +64,33 @@ ingredient_t *create_ingredient(char *name, float cost) {
 
     obj->name = name;
     obj->cost = cost;
+
+    return obj;
+}
+
+recipe_t *create_recipe(char *name, ingredient_t **ingredients, size_t num_ingredients) {
+    recipe_t *obj = malloc(sizeof(recipe_t));
+    if (obj == NULL) {
+        return NULL;
+    }
+    obj->name = name;
+    obj->ingredients = ingredients;
+    obj->num_ingredients = num_ingredients;
+    obj->recipe_cost = calculate_recipe_cost(obj);
+
+    return obj;
 }
 
 int main(void) {
+    ingredient_t *cucumber_ptr = create_ingredient("cucumber", 1.25);
+    ingredient_t *rice_ptr = create_ingredient("rice", 0.75);
+
+    ingredient_t *arr[] = {cucumber_ptr, rice_ptr};
+
+    recipe_t *sample_r = create_recipe("sample", arr, 2);
+
+    free(cucumber_ptr);
+    free(rice_ptr);
+    free(sample_r);
 
 }
