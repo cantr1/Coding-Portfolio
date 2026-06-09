e2807464b516a5c377362570f93853246c2fc309
Kelly Cantrell <kellycantrelly@gmail.com>
2026-06-09 18:22:53 -0500

    Work with maps

--- Clojure/basics.clj
+++ Clojure/basics.clj
@@ -9,3 +9,17 @@
 (defn hello [name] (str "Hello, " name))
 ;hello "GitHub"
 
+(defn divide [x y] (quot x y))
+;divide 25 5
+
+; Vectors = [1 2 3], sequential data
+; List = (1 2 3), can be quoted with = (quote (1 2 3)) or `(1 2 3)
+; Set = {2 5 6 1}, no dupes
+; Maps = {:language "Clojure" :favorite false}
+
+; Work with Maps
+(defrecord ProgrammingLanguage [language favorite])
+(->ProgrammingLanguage "Clojure" false)
+
+;(get ProgrammingLanguage :name)
+;more idomatic: (:name ProgrammingLanguage)
