76f54cc44dc415376936e55e4a2b122aa35ac2ad
Kelly Cantrell <kellycantrelly@gmail.com>
2026-07-04 14:25:37 -0500

    Initial DB usage implemented - creates users and deletes the table with reset endpoint

--- Go/learn_webservers/main.go
+++ Go/learn_webservers/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"slices"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"example.com/learn_web_servers/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	dbQueries      database.Queries
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -27,7 +43,19 @@ func (cfg *apiConfig) resetServerHits() {
 }
 
 func main() {
+	godotenv.Load() // load env vars
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+
+	// open connection to database
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Failure to open connection to backend DB")
+		return
+	}
+
 	var apiCfg = apiConfig{}
+	apiCfg.dbQueries = *database.New(db) // attach db queries so handlers can access
 	const port = ":8080"
 	const filepathRoot = "./web/"
 
@@ -60,10 +88,17 @@ func main() {
 
 	// Reset
 	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, req *http.Request) {
+		if platform != "dev" {
+			http.Error(w, "Application not in dev environment", http.StatusForbidden)
+		}
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // Normal header
-		w.WriteHeader(http.StatusOK)
 		apiCfg.resetServerHits()
-		io.WriteString(w, "Reset Server hits to 0\n")
+		err := apiCfg.dbQueries.RemoveUsers(req.Context())
+		if err != nil {
+			http.Error(w, "Failure to erase SQL DB", http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "Reset Server hits to 0 and erased DB\n")
 	})
 
 	// Validate POST Data
@@ -84,7 +119,7 @@ func main() {
 			Body string `json:"cleaned_body"`
 		}
 
-		w.Header().Set("Content-Type", "application/json") // All repsonses will be JSON
+		w.Header().Set("Content-Type", "application/json") // All responses will be JSON
 
 		decoder := json.NewDecoder(req.Body)
 		params := parameters{}
@@ -96,10 +131,10 @@ func main() {
 			}
 			dat, err := json.Marshal(respBody)
 			if err != nil {
-				log.Printf("Error marshalling JSON: %s", err)
+				http.Error(w, "Error marshaling JSON string", http.StatusInternalServerError)
 			}
 
-			w.WriteHeader(500)
+			w.WriteHeader(400)
 			w.Write(dat)
 			return
 		}
@@ -144,6 +179,53 @@ func main() {
 			return
 		}
 		w.WriteHeader(200)
+		w.Write(dat)
+	})
+
+	// Create User
+	mux.HandleFunc("POST /api/users", func(w http.ResponseWriter, req *http.Request) {
+		type parameters struct {
+			Email string `json:"email"`
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		// decode JSON
+		decoder := json.NewDecoder(req.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		if params.Email == "" {
+			http.Error(w, "Email is required", http.StatusBadRequest)
+			return
+		}
+
+		dbUser, err := apiCfg.dbQueries.CreateUser(req.Context(), params.Email)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		// Write dbUser data to our controlled struct
+		// to avoid over exposing data via the API
+		user := User{
+			ID:        dbUser.ID,
+			CreatedAt: dbUser.CreatedAt,
+			UpdatedAt: dbUser.UpdatedAt,
+			Email:     dbUser.Email,
+		}
+
+		dat, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, "Error constructing JSON", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(201)
 		w.Write(dat)
 	})
 
