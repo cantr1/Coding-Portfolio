42cdda48f38bcf88b5fe81c3624c9f5294806343
Kelly Cantrell <kellycantrelly@gmail.com>
2026-07-08 18:23:46 -0500

    Adding a leetcode directory for useful solutions and key concepts

---
+++ Leetcode/two_sum.py
@@ -0,0 +1,17 @@
+
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    """
+    This solution works by creating a hash map of the value
+    of seen numbers and their index.
+    By calculating the number we need, we can check if we have 
+    seen the value previously and return its index.
+    This greatly reduces the time complexity.
+    """
+    seen_numbers = {}
+    for i, num in enumerate(nums):
+        num_we_need = target - num
+        if num_we_need in seen_numbers:
+            return [seen_numbers[num_we_need], i]
+        seen_numbers[num] = i
