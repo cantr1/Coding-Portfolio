ada61bf4040704c32ffb6792104d5fa4ba38dd10
Kelly Cantrell <kellycantrelly@gmail.com>
2026-07-25 09:34:42 -0500

    added folder for experiments with test command

--- Go/learn_testing/cmd/main.go
+++ Go/learn_testing/cmd/main.go
@@ -1,15 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type myBigTestStruct struct {
+	collectedStrings map[int]testStruct
+}
+
+type testStruct struct {
+	sampleString string
+	charCount    map[rune]int
+}
+
+func (t testStruct) printCharCount() {
+	for key, value := range t.charCount {
+		fmt.Printf("Key: %s, Value: %d\n", string(key), value)
+	}
+}
+
+func (t testStruct) removeEmptySpaces() {
+	// This works ? pass by value vs pass by reference
+	// This is more to test / learn the deletion of keys
+	// can be avoided entriely by not writing to the map
+	// when an empty space is seen
+	delete(t.charCount, ' ')
+
+	// Seems to work because it is pass by reference
+}
+
+func (t *testStruct) countChars() {
+	t.charCount = make(map[rune]int) // this was uncommented but caused issues
+	for _, val := range strings.ToLower(t.sampleString) {
+		t.charCount[val]++ // this works because the zero value = 0
+	}
+}
 
 func main() {
-	fmt.Println("This module is used to test new features / scratchpad")
-	nums := []int{1, 2, 3}
+	var theBigOne myBigTestStruct = myBigTestStruct{}
 
-	a := nums[:2]
+	testString1 := "This module is used to test new features / scratchpad"
+	testString2 := "Right now I'm getting better with maps"
 
-	a = append(a, 100)
+	var firstTestStruct testStruct = testStruct{
+		sampleString: testString1,
+	}
+	firstTestStruct.countChars()
 
-	fmt.Println(nums)
-	fmt.Println(a)
+	// a more concise way to define similar to the above
+	secondTestStruct := testStruct{
+		sampleString: testString2,
+	}
+	secondTestStruct.countChars()
+
+	// Remove whitespace count - arbitrary action for learning
+	firstTestStruct.removeEmptySpaces()
+	secondTestStruct.removeEmptySpaces()
+
+	// Parse to the big one
+	theBigOne.collectedStrings = map[int]testStruct{
+		1: firstTestStruct,
+		2: secondTestStruct,
+	}
+
+	// Print the values of the maps
+	for key, value := range theBigOne.collectedStrings {
+		fmt.Printf("Struct #%d - String: %s\n", key, value.sampleString)
+		value.printCharCount()
+		fmt.Println()
+	}
 }
