16db24d4ec135a7ae05116e7873d60189ff8f53a
Kelly Cantrell <kellycantrelly@gmail.com>
2026-07-30 08:54:39 -0500

    Adding useful git aliases

---
+++ git_alias.sh
@@ -0,0 +1,19 @@
+# This file contains some common git alias commands I like to use
+
+# Returns the last commit info
+git config --global alias.last 'log -1 HEAD' # => git last
+
+# Returns the last few commits in one line style
+git config --global alias.recents 'log --oneline -n 5' # => git recents
+
+# Lazy status
+git config --global alias.st 'status' # => git st
+
+# Even lazier status
+git config --global alias.s 'status -s' # => git s
+
+# Branch work
+git config --global alias.br 'branch' # => git br -c new_branch
+
+# Checkout a branch
+git config --global alias.co 'checkout' # => git co new_branch
