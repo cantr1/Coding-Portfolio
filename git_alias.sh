222ecd3d493bcd45c8a9b183a9fe600da3d3fdd1
Kelly Cantrell <kellycantrelly@gmail.com>
2026-07-30 10:04:09 -0500

    Better alias, with color!

--- git_alias.sh
+++ git_alias.sh
@@ -1,10 +1,10 @@
 # This file contains some common git alias commands I like to use
 
 # Returns the last commit info
-git config --global alias.last 'log -1 HEAD' # => git last
+git config --global alias.last 'log -1 HEAD --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]"' # => git last
 
 # Returns the last few commits in one line style
-git config --global alias.recents 'log --oneline -n 5' # => git recents
+git config --global alias.recents 'log --oneline -n 5 --pretty=format:"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]"' # => git recents
 
 # Lazy status
 git config --global alias.st 'status' # => git st
@@ -17,3 +17,6 @@ git config --global alias.br 'branch' # => git br -c new_branch
 
 # Checkout a branch
 git config --global alias.co 'checkout' # => git co new_branch
+
+# To remove an alias
+# git config --global --unset alias.cmd
