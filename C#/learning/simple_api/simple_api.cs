12b728133536c53c87ff0382cfa3a3a886c20fdb
Kelly Cantrell <kellycantrelly@gmail.com>
2026-08-06 18:52:02 -0500

    Moving to controllers for simple API

--- C#/learning/simple_api/simple_api.cs
+++ C#/learning/simple_api/simple_api.cs
@@ -1,14 +1,36 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using DotNetEnv;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var port = builder.Configuration["PORT"] ?? "5000";
 var app = builder.Build();
 
-// Set the port and IP address binding to listen for all requests on the server IP
+// Set the port and IP address binding to listen for all requests on the server IP.
 app.Urls.Add($"http://0.0.0.0:{port}");
-app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+
+app.MapControllers();
 
 app.Run();
+
+namespace TestWebApi
+{
+    public class User
+    {
+        public required string Name { get; set; }
+        public required string Username { get; set; }
+    }
+
+    [ApiController]
+    [Route("/api/health")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult ReturnHealthStatus()
+        {
+            return Ok(new { status = "ok"});
+        }
+    }
+}
