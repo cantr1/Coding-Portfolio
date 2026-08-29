dc6a727d132b9f677752772f4d0cec87ea8be8db
Kelly Cantrell <kellycantrelly@gmail.com>
2026-08-29 12:53:38 -0500

    Better working TUI

--- Rust/rust_taskmanager/src/main.rs
+++ Rust/rust_taskmanager/src/main.rs
@@ -3,24 +3,28 @@ use std::io::Write;
 use std::path::Path;
 mod task;
 
-fn prompt_user_task_description() -> String {
-    print!("Enter task description: ");
-    io::stdout().flush().expect("failed to flush stdout");
+fn take_user_input(prompt: String) -> String {
+    print!("{}", prompt);
+    // ensure prompt displays immediately, not waiting for input
+    io::stdout().flush().expect("Failed to flush stdout");
 
+    // Create a mutable to contain inputs
     let mut user_input = String::new();
 
+    // Read from stdin
     io::stdin()
         .read_line(&mut user_input)
-        .expect("failed to read line");
+        .expect("Failed to read line");
 
+    // Clean input
     let cleaned_input = user_input.trim();
 
-    cleaned_input.to_string()
+    return cleaned_input.to_string();
 }
 
 fn create_new_task(task_stats: &mut task::TaskStatistics, task_list: &mut Vec<task::Task>) {
     // Get description of task from user
-    let task_description = prompt_user_task_description();
+    let task_description = take_user_input("Enter task description: ".to_string());
 
     // Create the task
     let new_task: task::Task = task::build_task(task_stats.next_task, &task_description, false);
@@ -30,6 +34,19 @@ fn create_new_task(task_stats: &mut task::TaskStatistics, task_list: &mut Vec<ta
 
     // Increment task statistics
     task::increment_next_task(task_stats);
+}
+
+fn view_task_list(task_list: &Vec<task::Task>) {
+    println!("");
+    for task in task_list {
+        println!(
+            "[{}] ID: {} - {}",
+            if task.complete { "*" } else { " " },
+            task.id,
+            task.description
+        )
+    }
+    println!("");
 }
 
 fn main() {
@@ -45,8 +62,17 @@ fn main() {
     );
     let mut task_list = task::build_task_list(task_list_file);
 
-    // Create a new task, add to list and increment stats
-    create_new_task(&mut task_stats, &mut task_list);
+    'main_loop: loop {
+        let user_choice = take_user_input(
+            "Choices\n-------\nd - display tasks\nn - create new task\nq - quit\n~:".to_string(),
+        );
+        match user_choice.as_str() {
+            "d" => view_task_list(&task_list),
+            "n" => create_new_task(&mut task_stats, &mut task_list),
+            "q" => break 'main_loop,
+            _ => println!("unrecognized input"),
+        }
+    }
 
     // End of operations, write statistics and task list
     _ = task::write_task_statistics(task_stats_file, &task_stats);
